21ea2f9291ab3159e3b4211d6b0d6b2c7d9b7a6c
Andre Geraseev <ageraseev@gmail.com>
2023-03-17 14:19:27 -0300

    mercadopago weebhook

--- mercado_livre_weebhook.py
+++ mercado_livre_weebhook.py
@@ -9,43 +9,79 @@ import json
 from pedidos.models import Pedido
 
 
-@csrf_exempt
-@require_POST
+# @csrf_exempt
+# @require_POST
+# def mercado_pago_webhook(request):
+#     mp = mercadopago.SDK(os.getenv('MERCADOLIVRETOKEN'))
+#     # Verifique se o request é válido
+#     if not request.body:
+#         return HttpResponse(status=400)
+#
+#     # Analise o JSON recebido
+#     try:
+#         data = json.loads(request.body)
+#     except json.JSONDecodeError:
+#         return HttpResponse(status=400)
+#
+#     # Verifique se existe um 'id' no JSON recebido
+#     if 'id' not in data:
+#         return HttpResponse(status=400)
+#
+#     # Obtenha o recurso da notificação usando a API do Mercado Pago
+#     resource = mp.get(f"/v1/payments/{data['id']}")
+#
+#     # Verifique se a resposta contém um objeto 'response'
+#     if 'response' not in resource:
+#         return HttpResponse(status=400)
+#
+#     # Obtenha o 'status' e o 'order_id' da resposta
+#     payment_status = resource['response']['status']
+#     order_id = resource['response']['order']['id']
+#
+#     # Atualize o status do Pedido com base no status do pagamento
+#     if payment_status == 'approved':
+#         try:
+#             pedido = Pedido.objects.get(mercado_pago_id=order_id)
+#             pedido.status = 'Pago'
+#             pedido.save()
+#         except Pedido.DoesNotExist:
+#             return HttpResponse(status=404)
+#
+#     # Retorne uma resposta com status 200 para confirmar o recebimento da notificação
+#     return HttpResponse(status=200)
 def mercado_pago_webhook(request):
     mp = mercadopago.SDK(os.getenv('MERCADOLIVRETOKEN'))
-    # Verifique se o request é válido
     if not request.body:
-        return HttpResponse(status=400)
+        return JsonResponse({'error': 'Request body is empty.'}, status=400)
 
-    # Analise o JSON recebido
     try:
         data = json.loads(request.body)
     except json.JSONDecodeError:
-        return HttpResponse(status=400)
+        return JsonResponse({'error': 'Invalid JSON format.'}, status=400)
 
-    # Verifique se existe um 'id' no JSON recebido
-    if 'id' not in data:
-        return HttpResponse(status=400)
+    if 'data' not in data or 'id' not in data['data']:
+        return JsonResponse({'error': 'Missing data or id in the request.'}, status=400)
 
-    # Obtenha o recurso da notificação usando a API do Mercado Pago
-    resource = mp.get(f"/v1/payments/{data['id']}")
+    if 'type' not in data:
+        return JsonResponse({'error': 'Missing type in the request.'}, status=400)
 
-    # Verifique se a resposta contém um objeto 'response'
+    if data['type'] != 'payment':
+        return JsonResponse({'error': 'Invalid type. Only "payment" type is supported.'}, status=400)
+
+    resource = mp.get(f"/v1/payments/{data['data']['id']}")
+
     if 'response' not in resource:
-        return HttpResponse(status=400)
+        return JsonResponse({'error': 'Missing response object in the API response.'}, status=400)
 
-    # Obtenha o 'status' e o 'order_id' da resposta
     payment_status = resource['response']['status']
     order_id = resource['response']['order']['id']
 
-    # Atualize o status do Pedido com base no status do pagamento
     if payment_status == 'approved':
         try:
             pedido = Pedido.objects.get(mercado_pago_id=order_id)
             pedido.status = 'Pago'
             pedido.save()
         except Pedido.DoesNotExist:
-            return HttpResponse(status=404)
+            return JsonResponse({'error': 'Order not found.'}, status=404)
 
-    # Retorne uma resposta com status 200 para confirmar o recebimento da notificação
-    return HttpResponse(status=200)
+    return JsonResponse({'success': 'Notification received and processed.'}, status=200)
