0c3417bb4f258da59122f6d88f5de9c259ba117b
Andre Geraseev <ageraseev@gmail.com>
2023-03-31 06:20:26 -0300

    arrumando agendado aviso

--- avise/tasks.py
+++ avise/tasks.py
@@ -1,13 +1,14 @@
 # tasks.py
 
-from celery import shared_task
-from produtos.models import Produto
 from avise.models import AvisoEstoque
 from avise.views import check_aviso_estoque
 
-@shared_task
+
 def check_aviso_estoque_task():
     avisos = AvisoEstoque.objects.filter(notificado=False)
     quantidade = 0
     for aviso in avisos:
         check_aviso_estoque(aviso, quantidade)
+
+if __name__ == '__check_aviso_estoque_task__':
+    check_aviso_estoque_task()
