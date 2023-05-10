7699b549c399e51b1f5dc0079adcea60c54ebdcc
Andre Geraseev <ageraseev@gmail.com>
2023-05-10 11:20:10 -0300

    tirando 404

--- xflavors/wsgi.py
+++ xflavors/wsgi.py
@@ -10,7 +10,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from xflavors import settings
+from django.core.wsgi import get_wsgi_application
+from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xflavors.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root=os.path.join(settings.BASE_DIR, 'media'), prefix='media/')
+
